e39d28ec85848d69370c2d8750a0b778d5c9f312
sunhz <rick.sun@yyinedu.com>
2022-07-16 17:54:11 +0800

    MOD: switch readme

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # FP Pieces
 
 Some functional programming style's codes.
+
+### Switch Case
+
+代替 `switch...case` 语句。
+
+```ts
+const demo = _switch(
+  (person) => person.type,
+  _case('student', (student) => student.grade),
+  _case('worker', (worker) => worker.job),
+)((person) => 'unknown:' + person.type)
+
+demo({ type: 'student', grade: 6 }) // 6
+demo({ type: 'worker', job: 'engineer' }) // engineer
+demo({ type: 'aliens' }) // unknown:aliens
+```
--- test/switch.test.ts
+++ test/switch.test.ts
@@ -2,48 +2,60 @@ import { _switch, _case } from '../source/switch'
 
 describe('switch', () => {
   test('run case if match', () => {
-    const result = _switch(
+    const fn = _switch(
       () => 'a',
       _case('a', () => 'a'),
       _case('b', () => 'b'),
     )()
 
-    expect(result()).toBe('a')
+    expect(fn()).toBe('a')
   })
 
   test('pass params to _if & _case', () => {
-    const result = _switch(
+    const fn = _switch(
       (num: number) => num % 2 === 0,
       _case(true, (num: number) => num),
       _case(false, (num: number) => num * 2),
     )()
 
-    expect(result(0)).toBe(0)
-    expect(result(1)).toBe(2)
-    expect(result(2)).toBe(2)
+    expect(fn(0)).toBe(0)
+    expect(fn(1)).toBe(2)
+    expect(fn(2)).toBe(2)
   })
 
   test('use _default if no _case match', () => {
-    const result = _switch(
+    const fn = _switch(
       (type) => type,
       _case('dog', () => 'dog'),
       _case('cat', () => 'cat'),
-    )((type: any) => `is ${type}`)
+    )((type) => `is ${type}`)
 
-    expect(result('dog')).toBe('dog')
-    expect(result('cat')).toBe('cat')
-    expect(result('pig')).toBe('is pig')
+    expect(fn('dog')).toBe('dog')
+    expect(fn('cat')).toBe('cat')
+    expect(fn('pig')).toBe('is pig')
   })
 
   test('throw error if no _case match and no _default', () => {
-    const result = _switch(
+    const fn = _switch(
       (type) => type,
       _case('dog', () => 'dog'),
       _case('cat', () => 'cat'),
     )()
 
-    expect(result('dog')).toBe('dog')
-    expect(result('cat')).toBe('cat')
-    expect(() => result('pig')).toThrow('No sure what to do with this option')
+    expect(fn('dog')).toBe('dog')
+    expect(fn('cat')).toBe('cat')
+    expect(() => fn('pig')).toThrow('No sure what to do with this option')
+  })
+
+  test('readme usage', () => {
+    const fn = _switch(
+      (person) => person.type,
+      _case('student', (student) => student.grade),
+      _case('worker', (worker) => worker.job),
+    )((person) => 'unknown:' + person.type)
+
+    expect(fn({ type: 'student', grade: 6 })).toBe(6)
+    expect(fn({ type: 'worker', job: 'engineer' })).toBe('engineer')
+    expect(fn({ type: 'aliens' })).toBe('unknown:aliens')
   })
 })
