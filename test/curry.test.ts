3778fde85e5fd68e320eb17c376ea308583b8bce
sunhz <rick.sun@yyinedu.com>
2022-07-19 20:15:25 +0800

    MOD: fix test

--- test/curry.test.ts
+++ test/curry.test.ts
@@ -14,7 +14,7 @@ describe('curry', () => {
   })
 
   test('return sub_func if not all params passed', () => {
-    expect(curry(oneParams)()()).toBe(5)
+    expect(curry(oneParams)()(5)).toBe(5)
     expect(curry(twoParams)(2)(12)).toBe(14)
     expect(curry(threeParams)(1)(2)(3)).toBe(6)
     expect(curry(threeParams)(5, 9)(10)).toBe(24)
